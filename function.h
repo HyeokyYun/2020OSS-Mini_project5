0ffc08ef384dacb95f6083162090b9be627a651f
Hyeok Yun <hyeoky98@gmail.com>
2020-04-19 00:27:26 +0900

    function.h

--- function.h
+++ function.h
@@ -1,6 +1,10 @@
-nclude "manager.h"
+#include "manager.h"
 #include <string.h>
 
 void saveData(Product *p, int count);
 int loadData(Product *p);
 int searchMenu();
+void searchByName(Product *p, int count);
+void searchByPrice(Product *p, int count);
+void searchByStars(Product *p, int count);
+void searchByWeight(Product *p, int count);
