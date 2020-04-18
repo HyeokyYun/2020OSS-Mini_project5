51259422a3b89c38e145b215efe23b362e2e0d3b
Hyeok Yun <hyeoky98@gmail.com>
2020-04-19 00:28:53 +0900

    function.c

--- function.c
+++ function.c
@@ -29,3 +29,42 @@ int loadData(Product *p){
 	printf("\n==> 저장된 제품목록을 로딩하였습니다.\n");
 	return count;
 }
+
+int searchMenu(){
+	int searchNum;
+	printf("\n===[제품검색메뉴]===\n");
+	printf("1. 이름으로 찾기\n");
+	printf("2. 가격으로 찾기\n");
+	printf("3. 평점으로 찾기\n");
+	printf("4. 중량으로 찾기\n");
+	printf("0. 검색기능 종료\n");
+	printf("=====================\n");
+	printf("==> 사용할 메뉴: ");
+	scanf("%d", &searchNum);
+	printf("=====================\n");
+	
+
+	return searchNum;
+}
+
+void searchByName(Product *p, int count){
+	int nameCount = 0;
+	char searchName[20];
+	printf("[이름으로 제품검색]\n");
+	printf("입력: ");
+	scanf("%s", searchName);
+	
+	printf("\n== = = [등록된 제품 목록] = = ==\n");
+	for(int i = 0; i < count; i++){
+		if(p[i].price == -1 && p[i].weight == -1)continue;
+		if(strstr(p[i].name, searchName)){
+			printf("이름에 \"%s\" 를 포함하는 제품(들)입니다.\n", searchName);
+			
+			readProduct2(p[i]);
+			nameCount++;
+		}
+	}
+	if(nameCount == 0){
+		printf("==> 이름에 \"%s\" 를 포함하는 제품은 없습니다.\n", searchName);
+	}
+}
