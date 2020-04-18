df0df785199389490b488f349c5d148f9496e6d4
Hyeok Yun <hyeoky98@gmail.com>
2020-04-19 00:32:51 +0900

    main.c

--- main.c
+++ main.c
@@ -17,3 +17,55 @@ int programMenu(){
 
 	return menuNum;
 }
+
+  while(1){
+	  choice = programMenu();
+	  if(choice == 0)break;
+	  else if(choice == 1){
+		  if(count > 0){
+			  listProduct(plist, curcount);
+		  }
+		  else {
+			printf("==> 등록된 제품이 없습니다.\n");
+			printf("==> 제품정보를 등록하세요.\n");
+		  }
+	  }
+	  else if(choice == 2){
+		  addProduct(&plist[curcount++]);
+		  count ++;
+	  }
+	  else if(choice == 3){
+		  if(count > 0){
+			int select = selectDataNo(plist, curcount);
+			if(select == 0){
+				printf("==> 수정이 취소됩니다.\n");
+				continue;
+			}
+			updateProduct(&plist[select - 1]);
+		  }
+		  else {
+			printf("==> 등록된 제품이 없습니다.\n");
+			printf("==> 제품정보를 등록하세요.\n");
+		  }
+	  }
+	  else if (choice == 4){
+		  char deleteok;
+		  if(count > 0){
+			int select = selectDataNo(plist, curcount);
+			if(select == 0){
+				printf("==> 삭제가 취소됩니다.\n");
+				continue;
+			}
+			printf("데이터를 삭제합니다. (Y/y)");
+		  	scanf("%s", &deleteok);
+		  	if(deleteok == 'y' || deleteok == 'Y'){
+			deleteProduct(&plist[select - 1]);
+			count--;
+		  	}
+		  	else printf("==> 제품삭제를 취소합니다.\n");
+	  	  }
+		  else {
+			printf("==> 등록된 제품이 없습니다.\n");
+			printf("==> 제품정보를 등록하세요.\n");
+		  }
+  	  }
